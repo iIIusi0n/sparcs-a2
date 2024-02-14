3b39f50699e4429df5f4e2077fbbf1224dc68367
iiiusi0n <iiiusi0n@wyvern.pw>
2024-02-15 04:15:52 +0900

    Add API tests for /user router

--- cmd/api-server-test/main.go
+++ cmd/api-server-test/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -96,4 +97,60 @@ func TestUserRouter() {
 	}
 
 	log.Println("User:", body)
+
+	var response struct {
+		ID          int    `json:"id"`
+		Username    string `json:"username"`
+		RealName    string `json:"real_name"`
+		Email       string `json:"email"`
+		PhoneNumber string `json:"phone_number"`
+		CreatedAt   string `json:"created_at"`
+	}
+	err := json.Unmarshal([]byte(body), &response)
+	if err != nil {
+		panic(err)
+	}
+
+	url = ServerURL + "/api/v1/user/" + strconv.Itoa(response.ID)
+	status, body = SendRequest("GET", url, "")
+	if status != 200 {
+		panic("Failed to get user: " + body)
+	}
+
+	log.Println("User:", body)
+
+	url = ServerURL + "/api/v1/user/"
+	response.Email = RandomString(10) + "@updated.com"
+	user, err := json.Marshal(response)
+	if err != nil {
+		panic(err)
+	}
+
+	status, body = SendRequest("PATCH", url, string(user))
+	if status != 200 {
+		panic("Failed to update user: " + body)
+	}
+
+	log.Println("User updated:", body)
+
+	url = ServerURL + "/api/v1/user/"
+	status, body = SendRequest("GET", url, "")
+	if status != 200 {
+		panic("Failed to get user: " + body)
+	}
+
+	log.Println("User:", body)
+
+	url = ServerURL + "/api/v1/user/"
+	status, body = SendRequest("POST", url, `{
+		"username": "`+RandomString(10)+`",
+		"real_name": "Test User",
+		"email": "",
+		"phone_number": "01012345678"
+	}`)
+	if status != 200 {
+		panic("Failed to create user: " + body)
+	}
+
+	log.Println("User created:", body)
 }
