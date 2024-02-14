02f93c6d9fc3e22950ff639abb9acbd01b615cee
iiiusi0n <iiiusi0n@wyvern.pw>
2024-02-14 19:48:20 +0900

    Add default database actions

--- data/init.go
+++ data/init.go
@@ -10,7 +10,42 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type ManagerModel interface{}
+type ManagerModel interface {
+	CreateUser(user *User) (int, error)
+	ReadUser(id int) (*User, error)
+	UpdateUser(user *User) error
+	DeleteUser(id int) error
+
+	CreateHospital(hospital *Hospital) (int, error)
+	ReadHospital(id int) (*Hospital, error)
+	UpdateHospital(hospital *Hospital) error
+	DeleteHospital(id int) error
+
+	CreateInHospital(inHospital *InHospital) error
+	ReadInHospital(hospitalID, userID int) (*InHospital, error)
+	UpdateInHospital(inHospital *InHospital) error
+	DeleteInHospital(hospitalID, userID int) error
+
+	CreateWaitingNumber(waitingNumber *WaitingNumber) error
+	ReadWaitingNumber(hospitalID, userID int) (*WaitingNumber, error)
+	UpdateWaitingNumber(waitingNumber *WaitingNumber) error
+	DeleteWaitingNumber(hospitalID, userID int) error
+
+	CreateChatRoom(chatRoom *ChatRoom) (int, error)
+	ReadChatRoom(id int) (*ChatRoom, error)
+	UpdateChatRoom(chatRoom *ChatRoom) error
+	DeleteChatRoom(id int) error
+
+	CreateChat(chat *Chat) (int, error)
+	ReadChat(id int) (*Chat, error)
+	UpdateChat(chat *Chat) error
+	DeleteChat(id int) error
+
+	CreateChatRead(chatRead *ChatRead) error
+	ReadChatRead(chatID, userID int) (*ChatRead, error)
+	UpdateChatRead(chatRead *ChatRead) error
+	DeleteChatRead(chatID, userID int) error
+}
 
 type manager struct {
 	db *sql.DB
--- data/user.go
+++ data/user.go
@@ -8,3 +8,51 @@ type User struct {
 	PhoneNumber string `json:"phone_number"`
 	CreatedAt   string `json:"created_at"`
 }
+
+func (m *manager) CreateUser(user *User) (int, error) {
+	query := "INSERT INTO users (username, real_name, email, phone_number) VALUES (?, ?, ?, ?)"
+	result, err := m.db.Exec(query, user.Username, user.RealName, user.Email, user.PhoneNumber)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
+
+func (m *manager) ReadUser(id int) (*User, error) {
+	query := "SELECT * FROM users WHERE id = ?"
+	row := m.db.QueryRow(query, id)
+
+	user := User{}
+	err := row.Scan(&user.ID, &user.Username, &user.RealName, &user.Email, &user.PhoneNumber, &user.CreatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (m *manager) UpdateUser(user *User) error {
+	query := "UPDATE users SET username = ?, real_name = ?, email = ?, phone_number = ? WHERE id = ?"
+	_, err := m.db.Exec(query, user.Username, user.RealName, user.Email, user.PhoneNumber, user.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *manager) DeleteUser(id int) error {
+	query := "DELETE FROM users WHERE id = ?"
+	_, err := m.db.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
