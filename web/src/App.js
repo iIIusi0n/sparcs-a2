5adf95245392556dbad2ad8b31f1ee29b94c56f4
80portisfound <songbongjune131@gmail.com>
2024-02-15 13:10:07 +0900

    라우터 초기

--- web/src/App.js
+++ web/src/App.js
@@ -2,12 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import Main from "./Main/Main";
 import MapMain from "./Map/MapMain";
-
+import { Route, Routes } from "react-router-dom";
 const App = () => {
   return (
-    <>
-      <MapMain></MapMain>
-    </>
+    <div>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route exact path="/map" element={<MapMain />} />
+      </Routes>
+    </div>
   );
 };
 
--- web/src/Map/MapMain.js
+++ web/src/Map/MapMain.js
@@ -3,6 +3,9 @@ import MapboxSmall from "../components/mapPage/MapboxSmall";
 import { useState } from "react";
 import Search from "../components/mapPage/Search";
 import searchIcon from "../components/icons/search.svg";
+import currentButtonIcon from "../components/icons/currentbutton.svg";
+import writeButtonIcon from "../components/icons/writebutton.svg";
+import NavigationBarMap from "../components/navigation/NavigationBarMap";
 
 function MapMain() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -15,26 +18,64 @@ function MapMain() {
     console.log("검색어:", searchTerm);
   };
   return (
-    <div style={{ position: "relative", top: "0", bottom: "0", width: "100%" }}>
-      <div>
-        <MapboxSmall style={{ postion: "absolute" }}></MapboxSmall>
-      </div>
+    <div>
       <div
-        style={{
-          position: "absolute",
-          top: "2%",
-          left: "8%",
-          display: "flex",
-        }}
+        style={{ position: "relative", top: "0", bottom: "0", width: "100%" }}
       >
-        <div style={{ position: "absolute", top: "28%" }}>
-          <img
-            src={searchIcon}
-            alt="Search Icon"
-            style={{ position: "absolute", width: "23px", left: "13px" }}
-          />
+        <div style={{ postion: "relative" }}>
+          <MapboxSmall></MapboxSmall>
         </div>
-        <Search value={searchTerm} onChange={handleSearchChange}></Search>
+        <div
+          style={{
+            position: "absolute",
+            top: "2%",
+            left: "8%",
+            display: "flex",
+          }}
+        >
+          <div style={{ position: "absolute", top: "23%", left: "1%" }}>
+            <img
+              src={searchIcon}
+              alt="Search Icon"
+              style={{ position: "absolute", width: "23px", left: "13px" }}
+            />
+          </div>
+          <Search value={searchTerm} onChange={handleSearchChange}></Search>
+          <button
+            style={{
+              position: "relative",
+              top: "630px",
+              left: "-390px",
+              margin: "0px",
+              padding: "0px",
+              border: "0px",
+            }}
+          >
+            <img src={currentButtonIcon} alt="Current Button Icon" />
+          </button>
+          <button
+            style={{
+              position: "relative",
+              top: "630px",
+              left: "-90px",
+              margin: "0px",
+              padding: "0px",
+              border: "0px",
+            }}
+          >
+            <img
+              src={writeButtonIcon}
+              alt="Write Button Icon"
+              style={{ width: "72" }}
+            />
+          </button>
+        </div>
+        <div style={{ position: "absolute", bottom: "50%" }}>
+          <NavigationBarMap></NavigationBarMap>
+        </div>
+        <div>
+          <NavigationBarMap></NavigationBarMap>
+        </div>
       </div>
     </div>
   );
