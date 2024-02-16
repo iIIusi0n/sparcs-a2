e7b100266be163e3d33b3ea1240671579ce9665d
80portisfound <songbongjune131@gmail.com>
2024-02-16 11:12:57 +0900

    ".....

--- web/src/components/pins/PinComponent.jsx
+++ web/src/components/pins/PinComponent.jsx
@@ -11,28 +11,28 @@ const PinComponent = (props) => {
       <div style={{ position: "fixed", bottom: "55%", left: "10%" }}>
         <ComponentsPinScroll
           value={value}
-          location={location}
-          locationAccount={locationAccount}
-          heart={heart}
-          walk={walk}
+          location={"유성병원"}
+          locationAccount={"북유성대로 93 · 1588-7011"}
+          heart={22}
+          walk={33}
         ></ComponentsPinScroll>
       </div>
       <div style={{ position: "fixed", bottom: "40%", left: "10%" }}>
         <ComponentsPinScroll
           value={value}
-          location={location}
-          locationAccount={locationAccount}
-          heart={heart}
-          walk={walk}
+          location={"한사랑의원"}
+          locationAccount={"봉명동 계룡로 150 "}
+          heart={20}
+          walk={30}
         ></ComponentsPinScroll>
       </div>
       <div style={{ position: "fixed", bottom: "25%", left: "10%" }}>
         <ComponentsPinScroll
           value={value}
-          location={location}
-          locationAccount={locationAccount}
-          heart={heart}
-          walk={walk}
+          location={"튼튼병원"}
+          locationAccount={"봉명동 559-4"}
+          heart={10}
+          walk={9}
         ></ComponentsPinScroll>
       </div>
     </div>
